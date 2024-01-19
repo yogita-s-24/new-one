fefc12d38255a028c4aaa387cc92df4b0e125b5e
Yogita Shete <yogitashete24@gmail.com>
2024-01-19 15:14:43 +0530

    user model created

---
+++ server/model/User.js
@@ -0,0 +1,30 @@
+import { Schema, model } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = model('User', userSchema);
+
+export default User;
+
