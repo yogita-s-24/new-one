8f714598c41f0b79e0eacfb0d2d55071f9400622
Yogita Shete <yogitashete24@gmail.com>
2024-01-19 11:44:34 +0530

    mongodb connected successfully

--- server/index.js
+++ server/index.js
@@ -7,6 +7,17 @@ dotevn.config();
 const app = express();
 app.use(express.json());
 
+
+const connectDB = () =>{
+    const conn = mongoose.connect(process.env.MONGO_URI);
+
+    if(conn){
+        console.log("MongoDb Connected Successfully.")
+    }
+}
+
+connectDB();
+
 const PORT = process.env.PORT;
 
 app.listen(PORT, ()=>{
