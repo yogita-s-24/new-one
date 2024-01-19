84f4c17a00cf5c78ac849398b0e2aeb56dd50658
Yogita Shete <yogitashete24@gmail.com>
2024-01-19 14:44:43 +0530

    feat : implement health api

--- server/index.js
+++ server/index.js
@@ -1,25 +1,28 @@
-import express from 'express'
-import mongoose from 'mongoose'
-import dotevn from 'dotenv'
+import express from "express";
+import mongoose from "mongoose";
+import dotevn from "dotenv";
 
 dotevn.config();
 
 const app = express();
 app.use(express.json());
 
+app.get("/healths", async (req, res) => {
+  res.send("Health is ok");
+});
 
-const connectDB = () =>{
-    const conn = mongoose.connect(process.env.MONGO_URI);
+const connectDB = () => {
+  const conn = mongoose.connect(process.env.MONGO_URI);
 
-    if(conn){
-        console.log("MongoDb Connected Successfully.")
-    }
-}
+  if (conn) {
+    console.log("MongoDb Connected Successfully.");
+  }
+};                                                                                                                             
 
 connectDB();
 
 const PORT = process.env.PORT;
 
-app.listen(PORT, ()=>{
-    console.log(`Server is running on ${PORT}`);
-})
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
+});
